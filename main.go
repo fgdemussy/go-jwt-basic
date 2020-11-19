a53c1e6cdc66f6b4b8194c729b7f10dadfb88ea8
francois <francois@chipax.com>
2020-11-19 20:45:20 -0300

    basic POST login endpoint

--- main.go
+++ main.go
@@ -1,3 +1,31 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var router = gin.Default()
+
+type user struct {
+	ID       uint64 `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var u1 = &user{
+	ID:       1,
+	Username: "john",
+	Password: "pass",
+}
+
+func main() {
+	router.POST("/login", login)
+	log.Fatal(router.Run(":8080"))
+}
+
+func login(c *gin.Context) {
+	c.JSON(http.StatusOK, "hey")
+}

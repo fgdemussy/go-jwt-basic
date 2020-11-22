ff71a84d11d93101df077ffc1ebff62a26a753e1
francois <francois@chipax.com>
2020-11-22 20:43:11 -0300

    returns signed jwt

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
@@ -36,6 +39,29 @@ func login(c *gin.Context) {
 		c.JSON(http.StatusUnauthorized, "Please provide login credentials")
 		return
 	}
+	secret, ok := os.LookupEnv("ACCESS_SECRET")
+	if !ok {
+		log.Fatalln("undefined ACCESS_SECRET")
+	}
+	log.Printf("secret: %v", secret)
+	token, err := createToken(u1.ID, secret)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, token)
+}
 
-	c.JSON(http.StatusOK, u)
+func createToken(userID uint64, secret string) (string, error) {
+	var err error
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["user_id"] = userID
+	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, err := at.SignedString([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+	return token, nil
 }
